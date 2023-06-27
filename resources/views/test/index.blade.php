3bf08d1dc6c3fbd309840e4cdff6b2b2f5cdaf9b
arthit15688 <137166308+arthit15688@users.noreply.github.com>
2023-06-27 17:27:00 +0700

    week2

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<h1>
+ant<img src= "{{ $ant }}" width="180" height="100" >
+bird<img src= "{{ $bird }}" width="180" height="100" > 
+cat<img src= "{{ $cat }}" width="180" height="100"> 
+god<img src= "{{ $god }}" width="180" height="100" > 
+</h1>
+
+
+</body>
+</html>
