096119d34f4aa2638dbf369c31d6add2275e4218
arthit15688 <137166308+arthit15688@users.noreply.github.com>
2023-07-11 16:35:53 +0700

    week4

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -2,9 +2,9 @@
 <body>
 <h2>Arthit Rattanakul</h2>
 <h1>
-ant<a href=" gallery/ant "> <img src= "{{ $ant }}" width="180" height="100" ></a>
-bird<a href=" gallery/bird "> <img src= "{{ $bird }}" width="180" height="100" ></a>
-cat<a href=" gallery/cat "> <img src= "{{ $cat }}" width="180" height="100"></a>
+ant<a href=" newgallery/ant "> <img src= "{{ $ant }}" width="180" height="100" ></a>
+bird<a href=" newgallery/bird "> <img src= "{{ $bird }}" width="180" height="100" ></a>
+cat<a href=" newgallery/cat "> <img src= "{{ $cat }}" width="180" height="100"></a>
 god<img src= "{{ $god }}" width="180" height="100" > 
 </h1>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MyProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -115,3 +116,15 @@ Route::get("/tables/component", function () {
 Route::get("/themequiz" , function (){
 	return view("themequiz");
 });
+
+Route::get("/myprofile/create",[ MyProfileController::class , "create" ]);
+
+Route::get("/myprofile/{id}/edit", [ MyProfileController::class , "edit" ] );
+
+Route::get("/myprofile/{id}", [ MyProfileController::class , "show" ]);
+
+Route::get( "/coronavirus" ,[ MyProfileController::class , "coronavirus" ] );
+
+Route::get( "/newgallery" , [ MyProfileController::class , "gallery" ] );
+Route::get( "/newgallery/ant" , [ MyProfileController::class , "ant" ] );
+Route::get( "/newgallery/bird" , [ MyProfileController::class , "bird" ] );
